f448c8e3e09e8d41f4b8dbc1cae9084690675383
Lyka <bin.lyka@institute.pse.ngo>
2025-05-09 08:01:47 +0700

    home

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,9 @@ const App = () => {
     <div>
       {/* Header */}
       <Navbar />
+
       {/* main content */}
       <Routes>
-        {/* Defind the routes for the application here */}
-        <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/services" element={<h1>Services</h1>} />
-        <Route path="/contact" element={<h1>Contact</h1>} />
         <Route path="/card" element={<Card />} />
         <Route path="/" element={<Home />} />
       </Routes>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,14 @@
-import React from "react";
-
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="">
+        <div className="flex items-center justify-center  bg-gray-300">
+          <div className="w-150 h-145 bg-amber-300"></div>
+          <div className="w-150 h-145 bg-amber-600"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
