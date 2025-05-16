d16bb633b6d84067d160ec399856fc4a8d6e00fb
Lyka <bin.lyka@institute.pse.ngo>
2025-05-16 08:13:59 +0700

    add register

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/home";
 import Footer from "./layouts/footer";
 import Navbar from "./layouts/navbar";
 import Blog from "./pages/blog";
-
+import Register from "./pages/register";
 const App = () => {
   return (
     <div className="w-full h-screen ">
@@ -19,6 +19,7 @@ const App = () => {
         <Route path="/services" element={<h1>Services</h1>} />
         <Route path="/contact" element={<h1>Contact</h1>} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       {/* Footer */}
       <Footer />
