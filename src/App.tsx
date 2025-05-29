24c5b3c883bea7104e68dcfa1a797368fb78498a
Lyka <bin.lyka@institute.pse.ngo>
2025-05-29 08:25:12 +0700

    update projectroute

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-// import Navbar from "./layouts/navbar";
 import { Routes, Route } from "react-router";
+// public
 import Home from "./pages/home";
 import About from "./pages/about";
+import Blog from "./pages/blog";
+
 import Footer from "./layouts/footer";
 import Navbar from "./layouts/navbar";
-import Blog from "./pages/blog";
-import Register from "./Auth/Register";
+// private
 import Contact from "./pages/contact";
+import Register from "./Auth/Register";
 import Login from "./Auth/login";
+import ProtectedRoute from "./components/ProtectedRoute";
 const App = () => {
+  const isAuthenticated = false;
   return (
     <div className="w-full h-screen ">
       {/* Header */}
@@ -17,15 +21,36 @@ const App = () => {
       {/* main content */}
       <main className="mt-12">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/services" element={<h1>Services</h1>} />
+          {/* Public Routes */}
+          <Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blog" element={<Blog />} />
+          </Route>
+          {/* Private Routes */}
           <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<Blog />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          {/* Add more routes as needed */}
+
+          {/* Protected Routes */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute
+                element={<h1>Dashboard</h1>}
+                isAuthenticated={isAuthenticated}
+              />
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute
+                element={<h1>Profile</h1>}
+                isAuthenticated={isAuthenticated}
+              />
+            }
+          />
         </Routes>
       </main>
       {/* Footer */}
---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  element: JSX.Element;
+  isAuthenticated: boolean;
+}
+
+const ProtectedRoute = ({ element, isAuthenticated }: ProtectedRouteProps) => {
+  return isAuthenticated ? element : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
